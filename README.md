076b5824f879a523fa6ecc440fe413b21002d854
szorfein <szorfein@protonmail.com>
2019-02-09 07:51:59 -0300

    busybox seem ok for now - need more test

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # eclosion
 An initramfs for ZFS
+
+# Refs
+
++ https://wiki.gentoo.org/wiki/Custom_Initramfs
++ https://github.com/tokiclover/mkinitramfs-ll
+
--- eclosion.sh
+++ eclosion.sh
@@ -8,26 +8,54 @@ LUKS="no"
 
 [[ ! -d $WORKDIR ]] && mkdir $WORKDIR
 cd $WORKDIR
-mkdir -p bin lib dev etc mnt/root proc root sbin sys
+
+# Directory structure
+mkdir -p {bin,dev,etc,lib,lib64,mnt/root,proc,root,sbin,sys}
+# Device nodes
+cp -a /dev/{null,console,tty} dev/
+
 touch etc/mdev.conf
 if [[ $LUKS == "yes" ]] ; then
   mkdir -p share/gnupg
 fi
-cp -a /dev/{null,urandom,console,tty} .
-disks=$(lsblk -l | grep -e "^s" | awk '{print $1}')
-for disk in $disks ; do
-  echo "cp -a /dev/$disk ."
-done
 
-# Copy binaries | static install
+# Copy binaries | static install when possible
 source /etc/portage/make.conf
-if [ ! -f $WORKDIR/bin/busybox ] ; then
+
+#######################################################
+# Busybox
+
+BUSYBOX_BIN=$WORKDIR/bin/busybox
+if [ ! -x $BUSYBOX_BIN ] ; then
+  echo "[+] Install busybox"
   PKG=sys-apps/busybox
   BUSYBOX_EBUILD=$(ls /usr/portage/$PKG | head -n 1)
   USE="-pam static" ebuild /usr/portage/$PKG/$BUSYBOX_EBUILD clean unpack compile
-  (cp /var/tmp/portage/${PKG%/*}/${BUSYBOX_EBUILD%.*}/work/${BUSYBOX_EBUILD%.*}/busybox $WORKDIR/bin)
-  USE="-pam static" ebuild /usr/portage/$PKG/$BUSYBOX_EBUILD clean
+  (cp /var/tmp/portage/${PKG%/*}/${BUSYBOX_EBUILD%.*}/work/${BUSYBOX_EBUILD%.*}/busybox $BUSYBOX_BIN)
+  ebuild /usr/portage/$PKG/$BUSYBOX_EBUILD clean
+elif ldd $BUSYBOX_BIN >/dev/null ; then
+  echo "busybox is not static"
+  exit 1
+else
+  echo "[+] Busybox found"
 fi
+
+BUSY_BIN=$(type -p $BUSYBOX_BIN)
+BUSY_APPS=/tmp/busybox-apps
+$BUSY_BIN --list-full > $BUSY_APPS
+
+mv bin/busybox . && rm -rf bin/* && rm -rf sbin/*
+for bin in $(grep -e '^bin/[a-z]' $BUSY_APPS) ; do
+  ln -s busybox $bin 
+done
+for sbin in $(grep -e '^sbin/[a-z]' $BUSY_APPS) ; do
+  ln -s ../bin/busybox $sbin
+done
+rm bin/busybox
+mv busybox bin/
+
+#######################################################
+# Gnupg
 
 if [ ! -f $WORKDIR/bin/gnupg ] && [[ $LUKS == "yes" ]] ; then
   PKG=app-crypt/gnupg
@@ -43,7 +71,6 @@ zfs_bin="zfs zpool mount.zfs zdb fsck.zfs"
 module="zfs zavl zunicode icp zcommon znvpair spl"
 
 # TODO: copy binary and modules
-# TODO: make symbolic link of busybox static
 # TODO: install keymap for future use of gpg 
 # TODO: copy GCC deps libgcc_s.so.1
 
@@ -85,6 +112,6 @@ EOF
 chmod u+x init
 
 # Create the initramfs
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > /boot/eclosion-initramfs.img
+#find . -print0 | cpio --null -ov --format=newc | gzip -9 > /boot/eclosion-initramfs.img
 
 exit 0
